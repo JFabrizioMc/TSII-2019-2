240eff8579d457a44a7ec5274f3f2ce6a9d4e239
Jonathan Chavez <jce.jonathan@gmail.com>
2019-10-16 10:12:54 -0500

    .

--- Semana 08/parcial/editar_procesar.php
+++ Semana 08/parcial/editar_procesar.php
@@ -1,7 +1,17 @@
 <?php
 # Entrada
+$c = $_POST["p_codigo"];
+$n = $_POST["p_nombres"];
+$e = $_POST["p_correo"];
+$ca = $_POST["p_cargo"];
+$s = isset($_POST["p_sus"]) ? 1 : 0;
 
 # Proceso
+$pdo = new PDO("mysql:host=localhost;dbname=parcial;charset=utf8;", "root", "");
+$query = "UPDATE eventos SET nombres='$n', correo='$e', cargo='$ca', suscrito='$s' WHERE codigo='$c'";
+#die($query);
+$pdo->query($query);
 
 # Salida
+header("Location: index.php");
 ?>
